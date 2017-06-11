62be71de4bd52dcd0284c2c07131247371272eea
cgwu <gg.wcg@qq.com>
2017-06-11 16:53:25 +0800

    thymeleaf test.

---
+++ src/main/kotlin/com/dannis/testkotlin/domain/UserEntity.kt
@@ -0,0 +1,6 @@
+package com.dannis.testkotlin.domain
+
+/**
+ * Created by sam on 17-6-11.
+ */
+data class UserEntity(var name: String, var pwd: String)
--- src/main/kotlin/com/dannis/testkotlin/web/UserController.kt
+++ src/main/kotlin/com/dannis/testkotlin/web/UserController.kt
@@ -1,21 +1,48 @@
 package com.dannis.testkotlin.web
 
+import com.dannis.testkotlin.domain.UserEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.web.context.ContextLoader
+import javax.servlet.ServletContext
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpSession
+import org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext
+import org.springframework.web.context.WebApplicationContext
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
 
 /**
  * Created by sam on 17-6-11.
  */
 @Controller
 @RequestMapping("/user")
-class UserController {
+class UserController : ApplicationContextAware {
+
+    private var ctx: ApplicationContext? = null
+
+    override fun setApplicationContext(ctx: ApplicationContext?) {
+        this.ctx = ctx
+    }
 
     private val log = LoggerFactory.getLogger(UserController::class.java)
 
     @RequestMapping("/index")
-    fun index(): String {
-//        log.info("User Controller index 方法被调用")       // test OK!
+    fun index(request: HttpServletRequest, session: HttpSession): String {
+//        log.info("UserEntity Controller index 方法被调用")       // test OK!
+        session.setAttribute("sessionKey", "这是session里的值")
+        request.setAttribute("requestKey", "这是request里的值")
+        session.servletContext.setAttribute("servletCtxKey","这是Servlet Context值")
+        session.servletContext.setAttribute("now",Instant.now().atOffset(ZoneOffset.ofHours(8)))
+        session.servletContext.setAttribute("nowLocal", LocalDateTime.now())
+
+        var user = UserEntity("张三","123")
+        session.setAttribute("user",user)
         return "user/index"
     }
 
