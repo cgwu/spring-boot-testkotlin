2a30ed0403c3babbcb193414ad9330ff3ccf3fcd
foo <foo@example.com>
2017-06-15 12:46:12 +0800

    test send stomp message OK.

--- src/main/kotlin/com/dannis/testkotlin/config/WebSocketConfig.kt
+++ src/main/kotlin/com/dannis/testkotlin/config/WebSocketConfig.kt
@@ -2,12 +2,15 @@ package com.dannis.testkotlin.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.util.AntPathMatcher
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 
-// 参考: https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/html/websocket.html
 /*
+ * 官网参考: https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/html/websocket.html
+ * 示例参考: http://blog.csdn.net/haoyuyang/article/details/53364372
+ *
  * STOMP: Simple (or Streaming) Text Orientated Messaging Protocol.
  * Created by sam on 17-6-13.
  * */
@@ -15,17 +18,37 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 @EnableWebSocketMessageBroker
 class WebSocketConfig : AbstractWebSocketMessageBrokerConfigurer() {
 
+    // 端点的作用——客户端在订阅或发送消息到目的地址前，要连接该端点。
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/stomp").withSockJS()
+//        registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS();      // e.g.
     }
 
     override fun configureMessageBroker(config: MessageBrokerRegistry?) {
+        //应用程序以/app为前缀，代理目的地以/topic、/user为前缀
+        config!!.enableSimpleBroker("/topic", "/user")
 //        config!!.enableSimpleBroker("/topic", "/queue")
-        config!!.enableSimpleBroker("/topic")
+//        config!!.enableSimpleBroker("/topic")
 
-        config.setApplicationDestinationPrefixes("/app")
         /* The "/app" prefix is arbitrary. You can pick any prefix. It’s simply meant to differentiate messages
         to be routed to message-handling methods to do application work vs messages to be routed to
         the broker to broadcast to subscribed clients. */
+        config.setApplicationDestinationPrefixes("/app")
+
+        /*
+            Using Dot as Separator in @MessageMapping Destinations
+            Applications
+            can also switch to using "." (dot) instead of "/" (slash) as the separator in @MessageMapping mappings
+            by configuring a custom AntPathMatcher.
+            e.g.:
+            @Controller
+            @MessageMapping("foo")
+            public class FooController {
+                @MessageMapping("bar.{baz}")
+                public void handleBaz(@DestinationVariable String baz) { }
+            }
+            If the application prefix is set to "/app" then the foo method is effectively mapped to "/app/foo.bar.{baz}".
+        */
+        config.setPathMatcher(AntPathMatcher("."))
     }
 }
--- src/main/kotlin/com/dannis/testkotlin/web/WebSocketController.kt
+++ src/main/kotlin/com/dannis/testkotlin/web/WebSocketController.kt
@@ -1,7 +1,9 @@
 package com.dannis.testkotlin.web
 
 import com.dannis.testkotlin.component.WebSocketProducer
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.messaging.handler.annotation.*
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -18,11 +20,16 @@ wsProducer: WebSocketProducer
 
 @Controller
 @RequestMapping("/websocket")
+@MessageMapping("ws")
 class WebSocketController {
+    companion object {
+        private val log = LoggerFactory.getLogger(WebSocketController::class.java)
+    }
 
     @Autowired
     var producer: WebSocketProducer? = null
 
+    /* HTTP请求方法 */
     @RequestMapping("/msg-list/{chanel}")
     fun msgList(@PathVariable chanel: String, request: HttpServletRequest): String {
         request.setAttribute("userChanel", chanel)
@@ -35,4 +42,21 @@ class WebSocketController {
         producer!!.sendMessageTo(topic, message)
         return "OK-Sent"
     }
+
+    /* WebSocket请求方法 */
+    @MessageMapping("bar.{baz}")
+    fun handleBaz(@DestinationVariable baz: String) {
+        log.info("服务器接收到WebSocket消息: $baz")
+    }
+
+    @MessageMapping("logbody")
+    fun logMsg(@Payload body:String, @Header("priority") priority: String, @Headers headers: Map<String, Any>) {
+        log.info("服务器接收到WebSocket消息体内容: $body")
+        log.info("优先级: $priority")
+        log.info("所有头部: $headers")
+        producer!!.sendMessageTo("user1", body)
+    }
+
+
+
 }
